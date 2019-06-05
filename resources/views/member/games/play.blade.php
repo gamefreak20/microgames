7b53485801380cdc8cf021e3e933fade41623ff6
Matthijs Brinks <mbrinks@Administrators-iMac.local>
2019-06-05 15:35:04 +0200

    front-end can begin

---
+++ resources/views/member/games/play.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('css')
+@endsection
+
+@section('javascript')
+@endsection
+
+@section('lateJavascript')
+@endsection
+
+
+@section('content')
+
+
+
+@endsection
