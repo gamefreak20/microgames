179dae3d78a39674528c047f642e65db54cfe0fb
Thijs van Leeuwen <TLeeuwen@MakeAThon.lan>
2019-06-06 03:27:18 +0200

    index mostly done

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.main')
 
 @section('css')
+<link href="{{ asset('./css/index.css') }}" rel="stylesheet">
 @endsection
 
 @section('javascript')
+<script src="{{ asset('js/index.js') }}"></script>
 @endsection
 
 @section('lateJavascript')
@@ -12,15 +14,28 @@
 
 @section('content')
 
+  <div class="wrapper">
     @foreach($games as $game)
 
-        {{$game->name}}
-        <img src="{{asset('images/games/main/'.$game->id.'.png')}}" alt="image of {{$game->name}}">
 
-        @foreach($game->tags()->get() as $tag)
-            {{$tag->name}}
-        @endforeach
+        <a href=""><div class="innerDiv">
+          <div class="gameName">
+            {{$game->name}}
+          </div>
 
+          <div class="totalImg">
+            <div>
+              <img src="{{asset('images/games/main/'.$game->id.'.png')}}" alt="image of {{$game->name}}" class="banner">
+            </div>
+            <div class="underImage">
+            </div>
+          </div>
+
+          @foreach($game->tags()->get() as $tag)
+              {{$tag->name}}
+          @endforeach
+
+        </div></a>
     @endforeach
-
+  </div>
 @endsection
