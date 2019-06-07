a6666de2caa7a8710925630a46a2dbc67a14cf58
Bas Mensinga <BMensinga@MakeAThon.lan>
2019-06-07 10:30:31 +0200

    db

--- microgames.sql
+++ microgames.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2019 at 05:16 AM
+-- Generation Time: Jun 07, 2019 at 10:30 AM
 -- Server version: 10.1.40-MariaDB
 -- PHP Version: 7.3.5
 
@@ -50,7 +50,7 @@ CREATE TABLE `game_objects` (
   `game_pages_id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `order_number` int(11) NOT NULL,
   `kind` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `what` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `what` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -60,9 +60,25 @@ CREATE TABLE `game_objects` (
 --
 
 INSERT INTO `game_objects` (`id`, `game_pages_id`, `order_number`, `kind`, `what`, `created_at`, `updated_at`) VALUES
-(1, '040153ce-abb9-438f-b898-397fde47bfcf', 1, 'title', 'tes', '2019-06-07 00:54:01', '2019-06-07 00:54:01'),
-(2, '040153ce-abb9-438f-b898-397fde47bfcf', 2, 'text', 'stt', '2019-06-07 00:54:01', '2019-06-07 00:54:01'),
-(3, '040153ce-abb9-438f-b898-397fde47bfcf', 3, 'file', '040153ce-abb9-438f-b898-397fde47bfcf.3.PNG', '2019-06-07 00:54:01', '2019-06-07 00:54:01');
+(8, '6edee17b-7968-43f9-82f6-7ebe624923b0', 1, 'title', 'City Builder', '2019-06-07 05:08:05', '2019-06-07 05:08:05'),
+(9, '6edee17b-7968-43f9-82f6-7ebe624923b0', 2, 'text', 'The goal of the game is to build a city and to make a lot of money.\r\nYou can do this by placing down roads, buildings and raising the taxes.\r\nIf you place down a road, you can asign zones to that road. There are a number of three zones: residential, commercial, emergency. When you have assigned a zone, you will see that buildings start appearing on the zone. There is also demand. If you have build a lot of the same buildings, the demand of that building type goes down. If the demand of that building is to low, then that type building won\'t be build. To raise demand you need to place more of the other building types.\r\nAlso there is an emergency demand. If a building type of the emergency demand is to low, buildings will start deconstructing and leaving your city. To get more income you can raise the taxes. If you raise the taxes to much the people will be unhappy and start leaving. If you want to increase the taxes without people being angry, you need to increase the population by building more residential.', '2019-06-07 05:08:05', '2019-06-07 05:08:05'),
+(10, '6edee17b-7968-43f9-82f6-7ebe624923b0', 4, 'file', '6edee17b-7968-43f9-82f6-7ebe624923b0.3.PNG', '2019-06-07 05:08:05', '2019-06-07 05:08:05'),
+(11, 'aeccbac2-1204-42f0-bd92-e1537b73d88b', 1, 'title', 'Mole-a-thon', '2019-06-07 05:18:39', '2019-06-07 05:18:39'),
+(12, 'aeccbac2-1204-42f0-bd92-e1537b73d88b', 2, 'text', 'You are a very hungry mole that eats the wheat from the farmer. Now the farmer is following u and u need to run away in your tunnels. You can run away with W, A, S, D.', '2019-06-07 05:18:39', '2019-06-07 05:18:39'),
+(13, 'aeccbac2-1204-42f0-bd92-e1537b73d88b', 3, 'file', 'aeccbac2-1204-42f0-bd92-e1537b73d88b.3.png', '2019-06-07 05:18:39', '2019-06-07 05:18:39'),
+(14, '040153ce-abb9-438f-b898-397fde47bfc1', 1, 'title', 'Super card battles pro', '2019-06-07 05:22:24', '2019-06-07 05:22:24'),
+(15, '040153ce-abb9-438f-b898-397fde47bfc1', 2, 'text', 'Description: Have fun card battles and outnumber your enemy card!\r\n\r\nWin from your opponent by playing higher cards then your opponent\'s card! All enemies have different hp, and card throws so be carefull on wich cards to use on wich enemy. The dodge card is used to skip a turn.', '2019-06-07 05:22:24', '2019-06-07 05:22:24'),
+(16, '040153ce-abb9-438f-b898-397fde47bfc1', 3, 'text', 'Controls: Mouse only', '2019-06-07 05:22:24', '2019-06-07 05:22:24'),
+(17, '040153ce-abb9-438f-b898-397fde47bfc1', 4, 'file', '040153ce-abb9-438f-b898-397fde47bfc1.4.PNG', '2019-06-07 05:22:24', '2019-06-07 05:22:24'),
+(18, 'e3d5a3ca-70e4-4bc8-8b31-17884747f950', 1, 'title', 'Music run', '2019-06-07 05:29:56', '2019-06-07 05:29:56'),
+(19, 'e3d5a3ca-70e4-4bc8-8b31-17884747f950', 2, 'text', 'Description: Get as high as score as possible and have fun.', '2019-06-07 05:29:56', '2019-06-07 05:29:56'),
+(20, 'e3d5a3ca-70e4-4bc8-8b31-17884747f950', 3, 'text', 'Controls: use A, D to move and use the mouse to navigate through the menu\'s.', '2019-06-07 05:29:56', '2019-06-07 05:29:56'),
+(21, 'e3d5a3ca-70e4-4bc8-8b31-17884747f950', 4, 'file', 'e3d5a3ca-70e4-4bc8-8b31-17884747f950.4.PNG', '2019-06-07 05:29:56', '2019-06-07 05:29:56'),
+(22, 'a68c531f-7649-4846-83dc-2861b9148e2e', 1, 'title', 'Compupunch', '2019-06-07 05:35:51', '2019-06-07 05:35:51'),
+(23, 'a68c531f-7649-4846-83dc-2861b9148e2e', 2, 'text', 'Description: your computer has crashed for the fifth time in a row and you\'ve had it,  take out your anger by punching it really hard.', '2019-06-07 05:35:51', '2019-06-07 05:35:51'),
+(24, 'a68c531f-7649-4846-83dc-2861b9148e2e', 3, 'text', 'Controls: Mouse to do everything', '2019-06-07 05:35:51', '2019-06-07 05:35:51'),
+(25, 'a68c531f-7649-4846-83dc-2861b9148e2e', 4, 'file', 'a68c531f-7649-4846-83dc-2861b9148e2e.4.png', '2019-06-07 05:35:51', '2019-06-07 05:35:51'),
+(26, '6edee17b-7968-43f9-82f6-7ebe624923b0', 2, 'text', 'Controls:\r\nW/A/S/D to move the camera\r\nRight click to deselect the current building or road', '2019-06-07 05:08:05', '2019-06-07 05:08:05');
 
 -- --------------------------------------------------------
 
@@ -84,7 +100,11 @@ CREATE TABLE `game_pages` (
 --
 
 INSERT INTO `game_pages` (`id`, `name`, `slug`, `user_id`, `created_at`, `updated_at`) VALUES
-('040153ce-abb9-438f-b898-397fde47bfcf', 'Super Card Battles Pro', 'super-card-battles-pro', '60ce3e09-effc-4592-806a-97d2583e741b', '2019-06-06 23:44:11', '2019-06-06 23:44:11');
+('040153ce-abb9-438f-b898-397fde47bfc1', 'Super Card Battles Pro', 'super-card-battles-pro', '60ce3e09-effc-4592-806a-97d2583e741b', '2019-06-06 23:44:11', '2019-06-06 23:44:11'),
+('6edee17b-7968-43f9-82f6-7ebe624923b0', 'City Builder', 'city-builder', '60ce3e09-effc-4592-806a-97d2583e741b', '2019-06-07 04:38:18', '2019-06-07 04:38:18'),
+('a68c531f-7649-4846-83dc-2861b9148e2e', 'Compupunch', 'compupunch', '60ce3e09-effc-4592-806a-97d2583e741b', '2019-06-07 05:33:38', '2019-06-07 05:33:38'),
+('aeccbac2-1204-42f0-bd92-e1537b73d88b', 'moleman', 'moleman', '60ce3e09-effc-4592-806a-97d2583e741b', '2019-06-07 05:13:39', '2019-06-07 05:13:39'),
+('e3d5a3ca-70e4-4bc8-8b31-17884747f950', 'musicrun', 'musicrun', '60ce3e09-effc-4592-806a-97d2583e741b', '2019-06-07 02:06:29', '2019-06-07 02:06:29');
 
 -- --------------------------------------------------------
 
@@ -179,6 +199,7 @@ CREATE TABLE `model_has_roles` (
 --
 
 INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
+(1, 'MicroGames\\User', '17513c2c-693e-49bb-a38f-0ccaa41c1389'),
 (5, 'MicroGames\\User', '60ce3e09-effc-4592-806a-97d2583e741b');
 
 -- --------------------------------------------------------
@@ -326,7 +347,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `first_name`, `last_name`, `email`, `email_verified_at`, `password`, `provider_id`, `provider`, `profile_picture`, `level`, `exp`, `remember_token`, `created_at`, `updated_at`) VALUES
-('60ce3e09-effc-4592-806a-97d2583e741b', 'Joey', 'joey', 'stil', 'joeystil3@gmail.com', NULL, '$2y$10$iLpMZwY5DL7cCjMN3c77K.VHkEI9enlgvLwBts3FQaTNV1WCDY2lO', NULL, NULL, 0, 19, 750, NULL, '2019-06-06 13:06:30', '2019-06-06 21:42:06');
+('17513c2c-693e-49bb-a38f-0ccaa41c1389', 'Joey', 'joey', 'stil', 'joeystil3@gmail.com', NULL, '$2y$10$01Yo8HpYBXsRoOQMUKfH5uNOECdERDnhLplboDmUgSi5ossteLYUK', NULL, NULL, 0, 0, 0, NULL, '2019-06-07 06:15:43', '2019-06-07 06:15:43');
 
 --
 -- Indexes for dumped tables
@@ -446,7 +467,7 @@ ALTER TABLE `banneds`
 -- AUTO_INCREMENT for table `game_objects`
 --
 ALTER TABLE `game_objects`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `game_pages_tags`
